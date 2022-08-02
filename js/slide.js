c7dac730bc97ce15e842bef990373dcb90f6ae37
Gabriel de Menezes <gabriel.gmnogueira@gmail.com>
2022-08-01 23:10:19 -0300

    calculos de distância quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -1,28 +1,43 @@
 export default class Slide {
   constructor(slide, wrapper) {
-    this.slide = document.querySelector(slide);
+    this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper);
+    this.distance = { finalPosition: 0, startX: 0, movement: 0 }
   }
 
-  onsStart(event) {
+  moveSlide(distanceX) {
+    this.distance.movePosition = distanceX;
+    this.slide.style.transform = `translate3d(${distanceX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.distance.movement = (this.distance.startX - clientX) * 1.6;
+    return this.distance.finalPosition - this.distance.movement;
+  }
+
+  onStart(event) {
     event.preventDefault();
+    this.distance.startX = event.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
   onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
     this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.distance.finalPosition = this.distance.movePosition;
   }
 
   addSlideEvents() {
-    this.wrapper.addEventListener('mousedown', this.onsStart);
+    this.wrapper.addEventListener('mousedown', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
   }
 
   bindEvents() {
-    this.onsStart = this.onsStart.bind(this);
+    this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
   }
