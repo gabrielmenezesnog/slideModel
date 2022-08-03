486c60250a63c260fd53c00ed28e91f7a2085fb8
Gabriel de Menezes <gabriel.gmnogueira@gmail.com>
2022-08-02 21:08:49 -0300

    prev and next navigation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-import Slide from './slide.js';
-const slide = new Slide('.slide', '.slide-wrapper', 'active');
-slide.init();
+import { SlideNav } from './slide.js';
 
-slide.changeSlide(3);
-slide.activePrevSlide();
+const slide = new SlideNav('.slide', '.slide-wrapper', 'active');
+slide.init();
+slide.addArrow('.prev', '.next');
